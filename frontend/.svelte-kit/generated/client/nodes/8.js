7e7d92bcaae8718ca2ec32410f015ef2c5d7ec10
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2026-05-07 20:39:06 -0500

    Untrack node_modules, .svelte-kit, and .turbo (already in .gitignore)

--- frontend/.svelte-kit/generated/client/nodes/8.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/reset-password/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/reset-password/+page.svelte";
